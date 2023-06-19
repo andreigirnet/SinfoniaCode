1776ec5b6db6999229fa021db353f2c442fbc923
ASUS TUF GAMING <adnrei.girnet96@gmail.com>
2023-06-19 20:39:53 +0300

    m

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Access-Control-Allow-Origin" content="*" />
         <title>Sinfonia Global Exchange</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
